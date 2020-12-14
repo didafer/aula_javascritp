7858557e85922e210024a95040a1d697ec99e9db
didafer <71114349+didafer@users.noreply.github.com>
2020-12-14 00:48:00 -0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var nome = "Fernando"
+var n1 = 3
+var n2 = 5
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos.")
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase());
+//alert(frase.replace("Japão", "Brasil"));
